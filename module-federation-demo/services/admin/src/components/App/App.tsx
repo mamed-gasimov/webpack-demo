62ff18e334cacef776517685dcdf38dc63def81b
mamed-gasimov <mamed.gasymov@gmail.com>
2024-06-10 16:59:46 +0400

    admin service

---
+++ module-federation-demo/services/admin/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+import { UserCard } from "@packages/shared/src/components/UserCard";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>ADMIN MODULE</h1>
+      <Outlet />
+      <UserCard username={"FROM ADMIN"} />
+    </div>
+  );
+};
---
+++ module-federation-demo/services/admin/src/router/Router.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+import { App } from "@/components/App/App";
+import { LazyAbout } from "@/pages/about/About.lazy";
+
+const routes = [
+  {
+    path: "/admin",
+    element: <App />,
+    children: [
+      {
+        path: "/admin/about",
+        element: (
+          <Suspense fallback={"Loading..."}>
+            <LazyAbout />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
+
+export default routes;
